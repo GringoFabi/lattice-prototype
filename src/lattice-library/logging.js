5077cb835e713ef2f0894c9124a0bbe982b467b6
fabian.muehlenbein <fabian15121998@googlemail.com>
2023-10-07 22:56:56 +0200

    V 0.2.4 - b - add persisting logs to the local storage - add support for persisting batches of logs which is triggered every   time the length of the persisted logs in the local storage surpasses   a certain threshold - upon persisting the local storage to the BE, on sucess, clear the   local storage

--- src/lattice-library/logging.js
+++ src/lattice-library/logging.js
@@ -1,9 +1,15 @@
-const logs = [];
+const logToConsole = true;
 const persistLogs = true;
+const localStorageLogs = true;
+const localStorageLimit = 100;
+
+const logs = [];
 const baseUrl = 'http://localhost:8000'
 
 export function log(action, node) {
-    console.log(`User Action ${action} at node ${node?.node}`)
+    if (logToConsole) {
+        console.log(`User Action ${action} at node ${node?.node}`)
+    }
     let log = {
         'action': action,
         'node': node,
@@ -12,7 +18,11 @@ export function log(action, node) {
     logs.push(log);
 
     if (persistLogs) {
-        saveSingeLog(log)
+        saveSingleLog(log)
+    }
+
+    if (localStorageLogs) {
+        writeLogToLocalStorage(log)
     }
 }
 
@@ -20,17 +30,46 @@ export function printLogs() {
     console.log(logs);
 }
 
-function saveSingeLog(log) {
+function saveSingleLog(log) {
     (async () => {
         const rawResponse = await fetch(baseUrl + '/log', buildBody({log: log}));
-        await handleResponse(rawResponse);
+        const content = await rawResponse;
+        if (!content.ok) {
+            console.error(new Error('Error persisting the log statement'), content);
+        }
     })();
 }
 
-export function saveLogs() {
+function writeLogToLocalStorage(log) {
+    let persistedLogs = localStorage.getItem('logs');
+
+    let localStorageLogs;
+    if (!persistedLogs) {
+        localStorageLogs = [log]
+    } else {
+        localStorageLogs = JSON.parse(persistedLogs);
+        localStorageLogs.push(log);
+    }
+
+    if (localStorageLogs.length >= localStorageLimit) {
+        saveLogs(localStorageLogs);
+        return
+    }
+
+    const jsonString = JSON.stringify(localStorageLogs);
+    localStorage.setItem('logs', jsonString);
+}
+
+function saveLogs(logs) {
     (async () => {
         const rawResponse = await fetch(baseUrl + '/logs', buildBody({logs: logs}));
-        await handleResponse(rawResponse);
+        const content = await rawResponse;
+        if (content.ok) {
+            console.log('Persisting Local Storage Logs was successful, clearing up some space');
+            localStorage.removeItem('logs');
+        } else {
+            console.error(new Error('Error persisting the Local Storage Logs'), content);
+        }
     })();
 }
 
@@ -45,11 +84,3 @@ function buildBody(payload) {
     };
 }
 
-async function handleResponse(rawResponse) {
-    const content = await rawResponse;
-    if (content.ok) {
-        console.log('Persisted log statement in backend', content);
-    } else {
-        console.error(new Error('Error persisting the log statement'), content);
-    }
-}
